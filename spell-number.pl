2a7a72a00743c50a1ff489ad36d140d7c99692c6
Andrew Shitov <andy@shitov.ru>
2017-10-18 12:43:13 +0200

    more

--- spell-number.pl
+++ spell-number.pl
@@ -1,38 +1,32 @@
-my $number = 1_014_167;
+my @names = <zero one two three four five six seven eight nine ten
+             eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen
+             twenty thirty forty fifty sixty seventy eighty ninety>;
 
-my @ones = <zero one two three four five six seven eight nine>;
-my @teens = <ten eleven twelve thirteen fourteen fifteen sixteen seventeen eightteen nineteen>;
-my @tens = <twenty thirty fourty fifty sixty seventy eighty ninety>;
+sub spell-number($number) {
+    my $n = $number.Int;
 
-my $base = 1_000_000;
-my @result;
-while $base >= 1 {
-    my $n = ($number / $base).Int;
-
-    if $n >= 100 {
-        my $hundreds = ($n / 100).Int;        
-        push @result, @ones[$hundreds] ~ ' hundred' if $hundreds;
-        $n %= 100;
-    }    
-
-    given $n {        
-        when * < 10 {
-            push @result, @ones[$n];
-        }
-        when 10 < * < 20 {
-            push @result, @teens[$n - 10];
-        }
-        when 20 < * < 100 {
-            my $tens = ($n / 10).Int;
-            my $ones = $n % 10;
-            my $r = @tens[$tens - 2];
-            $r ~= '-' ~ @ones[$ones] if $ones;
-            push @result, $r; 
-        }
+    my $r;
+    if $n < 20 {
+        $r = @names[$n];
+    }
+    elsif $n < 100 {        
+        $r = @names[$n / 10 + 18];
+        $r ~= '-' ~ @names[$n % 10] if $n % 10;
+    }
+    elsif $n < 1000 {        
+        $r = @names[$n / 100] ~ ' hundred';
+        $r ~= ' ' ~ spell-number($n % 100) if $n % 100;
+    }
+    else {
+        $r = spell-number($n / 1000) ~ ' thousand';
+        $r ~= ' ' ~ spell-number($n % 1000) if $n % 1000;
     }
 
-    $number %= $base;
-    $base /= 1000;
+    return $r;
 }
 
-say @result.join(' ');
+say spell-number(987654);  # nine hundred eighty-seven
+                           # thousand six hundred fifty-four
+say spell-number(0);       # zero
+say spell-number(17);      # seventeen
+say spell-number(100_001); # one hundred thousand one
