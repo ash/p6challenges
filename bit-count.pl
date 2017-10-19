e8ed593bf9985896dee003f040883d97285454d3
Andrew Shitov <andy@shitov.ru>
2017-10-20 00:16:19 +0200

    more

--- bit-count.pl
+++ bit-count.pl
@@ -1,5 +1,5 @@
 my $value = prompt('Enter value > ');
-$value = sprintf('%b', $value);
+$value = $value.Int.base(2);
 say "Binary representation: $value";
 $value ~~ s:g/0//;
 say "Number of 1s: {$value.chars}";
---
+++ dec2bin.pl
@@ -0,0 +1,3 @@
+say 42.base(2);  # 101010
+say 42.base(8);  # 52
+say 42.base(16); # 2A
