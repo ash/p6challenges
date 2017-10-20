063e7587ed13bd460736cdffc57980993f2b1c39
Andrew Shitov <andy@shitov.ru>
2017-10-20 09:54:11 +0200

    more

--- balanced-brackets.pl
+++ balanced-brackets.pl
@@ -1,23 +1,18 @@
 grammar Balanced {
     rule TOP {
-        | <expression>
-        | <balanced>
+        <expression>+
     }
     rule expression {
-        <:alpha>+?
-        [<expression> | <balanced>] ?
-        <expression>?
-    }
-    rule balanced {
-        '(' <expression> ')'
-        <expression>?
+        | <:alpha>+ <expression>?        
+        | '(' ~ ')' <expression>   
     }
 }
 
-my @tests = 'a', '(a)', '(a b c)', 'a (b)', '(b) a', '(b (a))', '((c))';
+my @tests = 'a', '(a)', '(a b c)', 'a (b)', '(b) a', '(b (a))', '( ( c))',
+            'a(b)c', 'a)', '(a) b c)', 'a b)', '(b a', '((b (a))', '((c)',
+            '(((a(((', 'a (' , ')a(';
 
-for @tests -> $test {
-    say $test;
-    say Balanced.parse($test);
-    say '';
+for @tests -> $test {    
+    my $result = Balanced.parse($test);
+    printf("%-12s is%s balanced\n", $test, $result ?? '' !! ' not');
 }
